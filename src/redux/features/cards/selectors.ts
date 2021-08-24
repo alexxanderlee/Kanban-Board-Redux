4f3b83de70c3bc47f2c1a52e5a5f5c536bd06910
Александр Ли <aleksandrli@Mac-mini-Sergey.local>
2021-08-24 14:42:58 +0600

    Rewrite card selectors

--- src/redux/features/cards/selectors.ts
+++ src/redux/features/cards/selectors.ts
@@ -4,16 +4,18 @@ import { ICard } from '../../../interfaces';
 
 const getCards = (state: RootState) => state.cards.items;
 
-const getId = (_: RootState, id: string) => id;
-
 export const getCardsByColumnId = createSelector(
-  getCards,
-  getId,
-  (cards, columnId) => cards.filter((card: ICard) => (card.columnId === columnId))
+  (state: RootState, columnId: string) => ({
+    cards: getCards(state),
+    columnId,
+  }),
+  ({ cards, columnId }) => cards.filter((card: ICard) => (card.columnId === columnId))
 );
 
 export const getCardById = createSelector(
-  getCards,
-  getId,
-  (cards, cardId) => cards.find((card: ICard) => (card.id === cardId))
+  (state: RootState, cardId: string) => ({
+    cards: getCards(state),
+    cardId,
+  }),
+  ({ cards, cardId }) => cards.find((card: ICard) => (card.id === cardId))
 );
