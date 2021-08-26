46f9e3b837ef1ac0419900826540534f1f172423
Александр Ли <aleksandrli@Mac-mini-Sergey.local>
2021-08-26 12:50:28 +0600

    Add final-from to auth page

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
+import { Form, Field } from 'react-final-form';
 import './Popup.css';
 import { useAppDispatch } from '../../redux/store';
 import { userActions } from '../../redux/features/user';
-
 
 interface PopupProps {
 }
@@ -10,39 +10,47 @@ interface PopupProps {
 const Popup: React.FC<PopupProps> = () => {
   const dispatch = useAppDispatch();
   const [popupVisible, setPopupVisible] = useState<boolean>(true);
-  const [errorInput, setErrorInput] = useState<boolean>(false);
-  const inputRef = useRef<HTMLInputElement>(null);
 
-  function submitInput(): void {
-    const value: string = inputRef.current!.value;
-    if (value === '') {
-      setErrorInput(true);
-      return;
-    }
-    setErrorInput(false);
-    dispatch(userActions.setUser(value));
+  interface FormValues {
+    username: string;
+  }
+
+  function onSubmit(values: FormValues) {
+    dispatch(userActions.setUser(values.username));
     setPopupVisible(false);
   }
 
-  function onKeyPress(event: React.KeyboardEvent): void {
-    if (event.key === 'Enter') {
-      submitInput();
-    }
-  }
+  const required = (value: string) => (value ? undefined : 'Required');
 
   return (
     <div className={popupVisible ? 'popup' : 'popup popup_hidden'}>
       <div className="popup__window">
-        <div className="popup__title">Enter your name</div>
-        <input
-          ref={inputRef}
-          type="text"
-          className={errorInput ? 'popup__input popup__input_error' : 'popup__input'}
-          onKeyPress={onKeyPress}
-          placeholder="Name"
-          autoFocus
+        <Form
+          onSubmit={onSubmit}
+          render={(props) => (
+            <form onSubmit={props.handleSubmit} className="auth-form">
+              <h2 className="auth-form__title">Enter your name</h2>
+              <Field name="username" validate={required} >
+                {({ input, meta }) => (
+                  <>
+                    {meta.error && meta.touched && <div className="auth-form__error">{meta.error}</div>}
+                    <input
+                      {...input}
+                      type="text"
+                      className={meta.error && meta.touched ? 'auth-form__input auth-form__input_error' : 'auth-form__input'}
+                      placeholder="Name"
+                    />
+                  </>
+                )}
+              </Field>
+              <button
+                type="submit"
+                className="auth-form__btn"
+                disabled={props.submitting || props.pristine}
+              >OK</button>
+            </form>
+          )}
         />
-        <button className="popup__btn" onClick={submitInput}>OK</button>
       </div>
     </div>
   );
